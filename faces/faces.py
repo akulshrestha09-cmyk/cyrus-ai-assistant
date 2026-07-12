e11222bce5cd30de97a5c9bfaeb65edf7be16895
akulshrestha09-cmyk <akulshrestha09@gmail.com>
2026-07-12 14:24:40 +0000

    Sun, Jul 12, 2026, 7:54 PM +05:30

--- faces/faces.py
+++ faces/faces.py
@@ -1,4 +1,12 @@
 def convert(text):
-    text=text.replace(":)",
-    text=text.replace(":(",""
+    text=text.replace(":)","🙂")
+    text=text.replace(":(","🙁")
+    return text
+def main():
+    user_input=input()
+    result=convert(user_input)
+    print(result)
+if __name__=="__main__":
+    main()
+
 
